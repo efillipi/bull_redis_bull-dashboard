71a0b48ff5abc593b355a17794a539f35f6dae34
Edney Fillipi <e.fillipi@gmail.com>
2021-10-13 18:57:52 -0300

    vai filhao

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import express from 'express'
+
+const app = express()
+
+const PORT = process.env.APP_PORT;
+
+app.listen(PORT, () => {
+  console.log(` 💻 Started: ${PORT}`);
+});
